e6db491759949d56a6e37bf36284fab55305f9d1
liuyue <liuyue2077@gmail.com>
2021-01-09 20:53:18 +0800

    polish: 组件分离

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { NavLink, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+import Home from './views/Home'
 import TransitonPage from './views/TransitionPage'
 import CSSTransitonPage from './views/CSSTransitonPage'
 import TransitionGroupPage from './views/TransitionGroupPage'
@@ -8,27 +9,12 @@ import SwitchTransitonPage from './views/SwitchTransitonPage'
 function App() {
   return (
     <div className="app pl-3 pr-3">
-      <h3 className="text-center mt-5 mb-3">React Transitoin Group</h3>
-      <ul className="list-group">
-        <li className="list-group-item disabled">Components</li>
-        <li className="list-group-item">
-          <NavLink to="transition">Transitoin Component</NavLink>
-        </li>
-        <li className="list-group-item">
-          <NavLink to="csstransiton">CSSTransiton Component</NavLink>
-        </li>
-        <li className="list-group-item">
-          <NavLink to="transitongroup">TransitionGroup Component</NavLink>
-        </li>
-        <li className="list-group-item">
-          <NavLink to="switchtransition">SwitchTransition Component</NavLink>
-        </li>
-      </ul>
       <Switch>
         <Route path="/transition" component={TransitonPage}/>
         <Route path="/csstransiton" component={CSSTransitonPage}/>
         <Route path="/transitongroup" component={TransitionGroupPage}/>
         <Route path="/switchtransition" component={SwitchTransitonPage}/>
+        <Route component={Home}/>
       </Switch>
     </div>
   )
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,24 @@
+import { NavLink} from 'react-router-dom'
+
 export default function Home() {
   return (
     <>
-      <h3>Home</h3>
+      <h3 className="text-center mt-5 mb-3">React Transitoin Group</h3>
+        <ul className="list-group">
+          <li className="list-group-item disabled">Components</li>
+          <li className="list-group-item">
+            <NavLink to="transition">Transitoin Component</NavLink>
+          </li>
+          <li className="list-group-item">
+            <NavLink to="csstransiton">CSSTransiton Component</NavLink>
+          </li>
+          <li className="list-group-item">
+            <NavLink to="transitongroup">TransitionGroup Component</NavLink>
+          </li>
+          <li className="list-group-item">
+            <NavLink to="switchtransition">SwitchTransition Component</NavLink>
+          </li>
+      </ul>
     </>
   )
 }
