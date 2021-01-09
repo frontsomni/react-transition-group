79b63afd2ecf2451b1028199238bda47c61def88
liuyue <liuyue2077@gmail.com>
2021-01-09 22:04:43 +0800

    add: Transitoin & SwitchTransiton

--- src/views/SwitchTransitonPage.tsx
+++ src/views/SwitchTransitonPage.tsx
@@ -1,7 +1,26 @@
+import './common.css'
+import {CSSTransition, SwitchTransition} from 'react-transition-group'
+import { useState } from 'react'
 export default function SwitchTransitonPage() {
+  const [state, setState] = useState(false)
   return (
     <>
-      <h3>有木有</h3>
+      <h3 className="text-center">SwitchTransiton</h3>
+      <SwitchTransition
+        mode="out-in"
+      >
+      <CSSTransition
+        key={state ? "Goodbye, world!" : "Hello, world!"}
+        addEndListener={(node, done) => node.addEventListener("transitionend", done, false)}
+        classNames='fade'
+      >
+        <button onClick={() => setState(state => !state)}
+          className="btn btn-primary"
+        >
+          {state ? "Goodbye, world!" : "Hello, world!"}
+        </button>
+      </CSSTransition>
+    </SwitchTransition>
     </>
   )
 }
--- src/views/TransitionPage.tsx
+++ src/views/TransitionPage.tsx
@@ -1,7 +1,23 @@
+import {Transition} from 'react-transition-group'
+import { useState } from "react"
+
 export default function TransitionPage() {
+  const [inStatus, setInStatus] = useState(false)
   return (
     <>
-      <h3>标题</h3>
+      <h3 className="text-center">Transiton</h3>
+      <button className="btn btn-primary"
+        onClick={() => setInStatus(!inStatus)}
+      >文本展示</button>
+      <Transition
+        in={inStatus}
+        timeout={400}
+        unmountOnExit
+      >
+        {
+          (state: any) => (<p className={`fade fade-${state}`}>状态：{inStatus ? '显示' : '隐藏'}</p>)
+        }
+      </Transition>
     </>
   )
 }
