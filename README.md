6ed94982d411688b4039fadd058a0ac0d39cbf35
liuyue <liuyue2077@gmail.com>
2021-01-17 12:39:44 +0800

    feat: 教程链接

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # React Transition Group 教程
-[传送门]()
+[教程传送门](https://readme.codermore.com/2021/01/17/React-Transition-Group-%E6%95%99%E7%A8%8B/)
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="home container text-center mt-3">
-      <a className="btn btn-primary" href="https://yuelau.github.io/react-transition-group/#/">传送门</a>
+      <a className="btn btn-primary" href="https://readme.codermore.com/2021/01/17/React-Transition-Group-%E6%95%99%E7%A8%8B/">教程传送门</a>
     </div>
   )
 }
